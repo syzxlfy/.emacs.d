a59a6bd7d31b6bec24ffce0df13e1b55a57bef55
syzxlfy <syzxgl@163.com>
2018-09-23 06:23:03 +0800

    .emacs.d
    2018-9-23 6:30

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
-
 ;;管理、更新、使用自已所用的Packages
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -31,14 +30,17 @@
 		       exec-path-from-shell
 		       popwin          ;;when require ,wh(setq company-minimum prefix-length 1) not require
 		       web-mode        ;; web-mode 则是一个非常常用很强大的用于编辑前端代码的 Major Mode,使用 M-;
-		                        ;;就可以注释当前行代码或选中行的代码。
+		       ;;就可以注释当前行代码或选中行的代码。
 		       expand-region   ;; 扩展选中区域
                        iedit
 		       org-pomodoro    ;;Org-pomodoro 是一个番茄时间工作法的插件。
 		       helm-ag
 		       helm-swoop
 		       magit
-		      ) "default packages")
+		       window-numbering	;窗口间切换 M-0 1 2 ...
+		       editorconfig     ;;格式化输入
+
+		       ) "default packages")
 
 (setq package-selected-packages lfy/packages)
 
@@ -68,7 +70,7 @@
 (setq frame-title-format
       '("" " LFY ☺ "
         (:eval (if (buffer-file-name)
-                   (abbreviate-file-name (buffer-file-name)) "%b"))))
+		   (abbreviate-file-name (buffer-file-name)) "%b"))))
 
 ;;保存时自动清除行尾空格及文件结尾空行
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -83,8 +85,8 @@
 (setq linum-format " %d ")
 ;; (add-hook 'org-mode-hook (lambda () (linum-mode 0)));;关闭org-mode的行号
 
-;;常用 C-a C-e C-d C-w 光标至 头 尾 向前删除 向后删除
-(global-set-key (kbd "C-w") 'backward-kill-word)
+;;常用 C-a C-e C-d C-w 光标至 头 尾 向前删除 向后删除, 后面设置了更高效的C-w
+;; (global-set-key (kbd "C-w") 'backward-kill-word)
 
 ;;逗号后自动加空格
 (global-set-key (kbd ",")
@@ -92,12 +94,11 @@
                     (interactive)
                     (insert ", ")))
 
-
 ;;;; 快速打开配置文件
 ;;启动后自动进入*scartch* buffer中，按 F5 后快速打开init.el文件，并按init.el配置
 (defun open-my-init-file()
-(interactive)
-(find-file "~/.emacs.d/init.el"))
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f5>") 'open-my-init-file) ;;这一行代码，将函数 open-init-file 绑定到 <F5> 键上
 
 (global-company-mode t) ;;自动补全
@@ -107,7 +108,6 @@
 ;; 2.Disable backup file
 ;;禁止备份文件
 (setq make-backup-files nil)
-
 
 ;; 3.Enable recentf-mode
 (require 'recentf)
@@ -132,7 +132,6 @@
 (global-hl-line-mode t)
 
 ;;装载monokai-thene 界面
-
 (load-theme 'monokai t)
 
 ;;装载hungry-delete(一种高效删除模式)
@@ -145,7 +144,7 @@
 					; when Smex is auto-initialized on its first run.
 (global-set-key (kbd "M-x") 'smex)
 
- ;;配置swiper,替代isearch,使用ivy来显示所有匹配的目录。
+;;配置swiper,替代isearch,使用ivy来显示所有匹配的目录。
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
@@ -175,11 +174,11 @@
 ;;实战 配置js2-mode and start to write javascript
 (require 'nodejs-repl)
 (setq auto-mode-alist
-     (append
-      '(("\\.js\\'" . js2-mode))
-      auto-mode-alist))
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
 (when (memq window-system '(mac ns))
-(exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize))
 
 ;;三个重要的快捷键：查找函数(C-h C-f)' 、查找变量(C-h C-v)、查找快捷键 (C-h C-k)
 (global-set-key (kbd "C-h C-f") 'find-function)
@@ -198,11 +197,11 @@
 (require 'org)
 (setq org-src-fonttify-natively t)
 
-;; 设置默认 Org Agenda 文件目录
+;; ;; 设置默认 Org Agenda 文件目录
 (setq org-agenda-files '("~/Lfy_Study/org"))
-;; 设置 org-agenda 打开快捷键
-;; 两个常用 C-c C-s scheduled items
-;;         C-c C-d set deadline of items
+;; ;; 设置 org-agenda 打开快捷键
+;; ;; 两个常用 C-c C-s scheduled items
+;; ;;         C-c C-d set deadline of items
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 ;;配置代码来设置一个模板（其中设置了待办事项的优先级还有触发键），可以实现快速记笔记
@@ -210,9 +209,83 @@
       '(("t" "Todo" entry (file+headline "~/Lfy_Study/org/gtd.org" "工作安排")
 	 "* TODO [#B] %?\n  %i\n"
 	 :empty-lines 1)))
-
 ;;绑定一个快捷键，r aka Remember
 (global-set-key (kbd "C-c r") 'org-capture)
+
+;;  (setq tramp-ssh-controlmaster-options
+;;         "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+;;   ;; define the refile targets
+;;   (defvar org-agenda-dir "" "gtd org files location")
+;;   (setq-default org-agenda-dir "~/org-notes")
+;;   (setq org-agenda-file-note (expand-file-name "notes.org" org-agenda-dir))
+;;   (setq org-agenda-file-gtd (expand-file-name "gtd.org" org-agenda-dir))
+;;   (setq org-agenda-file-journal (expand-file-name "journal.org" org-agenda-dir))
+;;   (setq org-agenda-file-code-snippet (expand-file-name "snippet.org" org-agenda-dir))
+;;   (setq org-default-notes-file (expand-file-name "gtd.org" org-agenda-dir))
+;;   (setq org-agenda-files (list org-agenda-dir))
+
+;;   (with-eval-after-load 'org-agenda
+;;     (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro)
+;;     ;; (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
+;;     ;;   "." 'spacemacs/org-agenda-transient-state/body)
+;;     )
+
+;; (setq org-agenda-files (list "~/org-notes/gtd.org"
+;; 			     "~/org-notes/notes.org"
+;; 			     "~/org-notes/journal.org"
+;; 			     "~/org-notes/snippet.org" ))
+
+
+;; ;; the %i would copy the selected text into the template
+;;   ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
+;;   ;;add multi-file journal
+;;   (setq org-capture-templates
+;;         '(("t" "Todo" entry (file+headline org-agenda-file-gtd "Workspace")
+;;            "* TODO [#B] %?\n  %i\n"
+;;            :empty-lines 1)
+;;           ("n" "notes" entry (file+headline org-agenda-file-note "Quick notes")
+;;            "* %?\n  %i\n %U"
+;;            :empty-lines 1)
+;;           ("b" "Blog Ideas" entry (file+headline org-agenda-file-note "Blog Ideas")
+;;            "* TODO [#B] %?\n  %i\n %U"
+;;            :empty-lines 1)
+;;           ("s" "Code Snippet" entry
+;;            (file org-agenda-file-code-snippet)
+;;            "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
+;;           ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
+;;            "* TODO [#A] %?\n  %i\n %U"
+;;            :empty-lines 1)
+;;           ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
+;;            "* TODO [#C] %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
+;;            :empty-lines 1)
+;;           ("l" "links" entry (file+headline org-agenda-file-note "Quick notes")
+;;            "* TODO [#C] %?\n  %i\n %a \n %U"
+;;            :empty-lines 1)
+;;           ("j" "Journal Entry"
+;;            entry (file+datetree org-agenda-file-journal)
+;;            "* %?"
+;;            :empty-lines 1)))
+
+;;An entry without a cookie is treated just like priority ' B '.
+;;So when create new task, they are default 重要且紧急
+;; (setq org-agenda-custom-commands
+;;       '(
+;;         ("w" . "任务安排")
+;;         ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
+;;         ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
+;;         ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
+;;         ("b" "Blog" tags-todo "BLOG")
+;;         ("p" . "项目安排")
+;;         ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"cocos2d-x\"")
+;;         ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"LFY\"")
+;;         ("W" "Weekly Review"
+;;          ((stuck "") ;; review stuck projects as designated by org-stuck-projects
+;;           (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
+;;           ))))
+
+
+
+
 
 
 ;;Org-pomodoro 是一个番茄时间工作法的插件（更多关于这个工作法的信息可以在这里找到）。 它的 GitHub 地址在这里。在 (require 'org-pomodoro) 后可以通过 ;;Customize-group 来对其进行设置，包括不同休息种类的时长。
@@ -282,23 +355,23 @@
 ;; Occur 与普通的搜索模式不同的是，它可以使用 Occur-Edit Mode (在弹出的窗口中按 e 进入编辑模式) 对搜索到的结果进行之间的编辑。
 ;; imenu 可以显示当前所有缓冲区的列表，下面的配置可以让其拥有更精确的跳转，
 (defun js2-imenu-make-index ()
-      (interactive)
-      (save-excursion
-	;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
-	(imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-				   ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
-				   ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
+  (interactive)
+  (save-excursion
+    ;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
+    (imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+			       ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
+			       ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
 (add-hook 'js2-mode-hook
-	      (lambda ()
-		(setq imenu-create-index-function 'js2-imenu-make-index)))
+	  (lambda ()
+	    (setq imenu-create-index-function 'js2-imenu-make-index)))
 (global-set-key (kbd "M-s i") 'counsel-imenu)
 
 ;; 将 expand-region 添加至我们的插件列表中，重启 Emacs 安装插件。再为其绑定一个快捷键，
@@ -338,7 +411,7 @@
 					    ("ept-" " @163.com")
                                             ;;gongyony
 					    ("syzx-" " @163.com")
-					   ))
+					    ))
 
 ;;ag它是非常快速的命令 行搜索工具，它是 Linux 的所有搜索工具中最快的。使用 ag 前我们需要进行安装，
 ;;# Windows 下通过 msys2 安装（确保在 path 中）
@@ -359,6 +432,75 @@
 ;;查看git状态，在init.el中添加快捷键绑定
 (global-set-key (kbd "C-x g") 'magit-status)
 
+
+;; 几个实用操作
+;; 1、 在行末或行中位置删除整行 ;;在emacs默认设置中，要想删除整行，需要先C-a跳到行首，然后使用C-k来删除整行，有些不方便。
+;;通过一下配置，可以使用C-w来删除整行（操作时不用事先选中整行）.
+;;同时不影响原有的剪切功能。M-w也可以在不事先选中整行的情况下复制整行。
+(defadvice kill-ring-save (before slickcopy activate compile)
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+(defadvice kill-region (before slickcut activate compile)
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
+;; 2、优化注释功能
+;;Emacs的默认设置中，M-;可以起到注释的作用，但是有不方便的地方：必须在选中一部分区域后才能进行注释。
+;;通过如下配置，可以达到以下效果：当光标位于行尾时，M-;在行尾进行注释；当光标位于其他位置时，M-;起到注释该行的作用；
+;;当选中一部分区域时，M-;起到注释该区域的作用。
+(defun qiang-comment-dwim-line (&optional arg)
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'qiang-comment-dwim-line)
+
+;; 3、设置透明效果
+;; 我觉得这个很有用，特别是需要一边写代码一边看参照其他文档时。按F12键可以一键切换透明度，非常方便
+;;set transparent effect
+(global-set-key [(f12)] 'loop-alpha)
+(setq alpha-list '((100 100) (95 65) (85 55) (75 45) (65 35)))
+(defun loop-alpha ()
+  (interactive)
+  (let ((h (car alpha-list)))                ;; head value will set to
+    ((lambda (a ab)
+       (set-frame-parameter (selected-frame) 'alpha (list a ab))
+       (add-to-list 'default-frame-alist (cons 'alpha (list a ab)))
+       ) (car h) (car (cdr h)))
+    (setq alpha-list (cdr (append alpha-list (list h))))
+    )
+  )
+;; 将子窗口自动编号,然后按 M-0…9跳转(最爱),  安装window-numbering.el,然后在.emacs中添加以下代码.
+;; 在emacs的默认设置中，需要使用C-x, o来进行窗口切换，颇为不便，特别是窗口数量比较大的时候。通过以下设置，可以
+;; 使用M-1/M-2/M-3/.../M-9来快速切换到第1/2/3/.../9窗口（窗口上有编号）。
+;; C-x 0：关闭当前窗口
+;; C-x 1：关闭当前窗口以为的其他窗口
+;; C-x 2：水平分割窗口
+;; C-x 3：垂直分割窗口
+;; C-x 4 0：关闭当前窗口和缓冲
+;; C-x 4 b：在另一窗口打开缓冲（如果当前只有一个窗口将分割一个新窗口）
+;; C-x 4 f：在另一窗口打开文件（…同上）
+;; C-x o：在多个窗口中循环切换
+(require 'window-numbering)
+(window-numbering-mode 1)
+;; If you want to affect the numbers, use window-numbering-before-hook or window-numbering-assign-func.
+;; For instance, to always assign the calculator window the number 9, add the following to your .emacs:
+(setq window-numbering-assign-func
+      (lambda () (when (equal (buffer-name) "*Calculator*") 9)))
+
+;;将代码格式化，设置快捷键 F6
+(require 'editorconfig)
+(editorconfig-mode 1)
+(global-set-key (kbd "<f6>") 'editorconfig-format-buffer)
+
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -366,6 +508,7 @@
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.08)
  '(company-minimum-prefix-length 1)
+ '(org-agenda-files nil)
  '(org-pomodoro-length 50))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
