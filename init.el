d072d7cc5108361b6212cb813b0dcc23e7c1553a
syzxlfy <syzxgl@163.com>
2018-09-23 13:08:21 +0800

    .emacs.d

--- init.el
+++ init.el
@@ -23,14 +23,14 @@
 		       hungry-delete   ;;饥饿删除包
 		       smex            ;;按 M-x 后 显示提示
 		       swiper          ;;按 C-s 后(搜索) 下面单独开个窗口给提示
-		       counsel         ;;???
+		       ;;counsel         ;;???
 		       smartparens     ;;智能成对匹配“” () [] {}
 		       js2-mode
 		       nodejs-repl
 		       exec-path-from-shell
 		       popwin          ;;when require ,wh(setq company-minimum prefix-length 1) not require
 		       web-mode        ;; web-mode 则是一个非常常用很强大的用于编辑前端代码的 Major Mode,使用 M-;
-		       ;;就可以注释当前行代码或选中行的代码。
+		                        ;;就可以注释当前行代码或选中行的代码。
 		       expand-region   ;; 扩展选中区域
                        iedit
 		       org-pomodoro    ;;Org-pomodoro 是一个番茄时间工作法的插件。
@@ -150,7 +150,6 @@
 (setq enable-recursive-minibuffers t)
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
-;;(global-set-key (kbd "<f6>") 'ivy-resume)
 ;;(global-set-key (kbd "M-x") 'counsel-M-x)
 ;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 ;;(global-set-key (kbd "C-h f") 'counsel-describe-function)
@@ -192,7 +191,6 @@
 ;;设置ORG-Mode 下自动换行，其它模式也可以
 (global-visual-line-mode 1)
 
-
 ;;使ORG文件中的源码能高亮显示。添加 Org-mode 文本内语法高亮
 (require 'org)
 (setq org-src-fonttify-natively t)
@@ -213,60 +211,54 @@
 ;; ;;绑定一个快捷键，r aka Remember
 ;; (global-set-key (kbd "C-c r") 'org-capture)
 
-;;子龙山人的ORG—GTD配置, 目录改为自己的
- (setq tramp-ssh-controlmaster-options
-        "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
-  ;; define the refile targets
-  (defvar org-agenda-dir "" "gtd org files location")
-  (setq-default org-agenda-dir "~/Lfy_Study/org/")
-  (setq org-agenda-file-note (expand-file-name "notes.org" org-agenda-dir))
-  (setq org-agenda-file-gtd (expand-file-name "gtd.org" org-agenda-dir))
-  (setq org-agenda-file-journal (expand-file-name "journal.org" org-agenda-dir))
-  (setq org-agenda-file-code-snippet (expand-file-name "snippet.org" org-agenda-dir))
-  (setq org-default-notes-file (expand-file-name "gtd.org" org-agenda-dir))
-  (setq org-agenda-files (list org-agenda-dir))
+;;;;子龙山人的ORG—GTD配置, 目录改为自己的
+(setq tramp-ssh-controlmaster-options
+      "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+;; define the refile targets
+(defvar org-agenda-dir "" "gtd org files location")
+(setq-default org-agenda-dir "~/Lfy_Study/org/")
+(setq org-agenda-file-note (expand-file-name "notes.org" org-agenda-dir))
+(setq org-agenda-file-gtd (expand-file-name "gtd.org" org-agenda-dir))
+(setq org-agenda-file-journal (expand-file-name "journal.org" org-agenda-dir))
+(setq org-agenda-file-code-snippet (expand-file-name "snippet.org" org-agenda-dir))
+(setq org-default-notes-file (expand-file-name "gtd.org" org-agenda-dir))
+(setq org-agenda-files (list org-agenda-dir))
 
-  (with-eval-after-load 'org-agenda
-    (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro)
-    ;; (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
-    ;;   "." 'spacemacs/org-agenda-transient-state/body)
-    )
-
-;; (setq org-agenda-files (list "~/org-notes/gtd.org"
-;; 			     "~/org-notes/notes.org"
-;; 			     "~/org-notes/journal.org"
-;; 			     "~/org-notes/snippet.org" ))
-
+(with-eval-after-load 'org-agenda
+  (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro)
+  ;; (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
+  ;;   "." 'spacemacs/org-agenda-transient-state/body)
+  )
 
 ;; ;; the %i would copy the selected text into the template
 ;;   ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
 ;;   ;;add multi-file journal
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline org-agenda-file-gtd "Workspace")
-           "* TODO [#B] %?\n  %i\n"
-           :empty-lines 1)
-          ("n" "notes" entry (file+headline org-agenda-file-note "Quick notes")
-           "* %?\n  %i\n %U"
-           :empty-lines 1)
-          ("b" "Blog Ideas" entry (file+headline org-agenda-file-note "Blog Ideas")
-           "* TODO [#B] %?\n  %i\n %U"
-           :empty-lines 1)
-          ("s" "Code Snippet" entry
-           (file org-agenda-file-code-snippet)
-           "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
-          ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
-           "* TODO [#A] %?\n  %i\n %U"
-           :empty-lines 1)
-          ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
-           "* TODO [#C] %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
-           :empty-lines 1)
-          ("l" "links" entry (file+headline org-agenda-file-note "Quick notes")
-           "* TODO [#C] %?\n  %i\n %a \n %U"
-           :empty-lines 1)
-          ("j" "Journal Entry"
-           entry (file+datetree org-agenda-file-journal)
-           "* %?"
-           :empty-lines 1)))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline org-agenda-file-gtd "Workspace")
+         "* TODO [#B] %?\n  %i\n"
+         :empty-lines 1)
+        ("n" "notes" entry (file+headline org-agenda-file-note "Quick notes")
+         "* %?\n  %i\n %U"
+         :empty-lines 1)
+        ("b" "Blog Ideas" entry (file+headline org-agenda-file-note "Blog Ideas")
+         "* TODO [#B] %?\n  %i\n %U"
+         :empty-lines 1)
+        ("s" "Code Snippet" entry
+         (file org-agenda-file-code-snippet)
+         "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
+        ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
+         "* TODO [#A] %?\n  %i\n %U"
+         :empty-lines 1)
+        ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
+         "* TODO [#C] %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
+         :empty-lines 1)
+        ("l" "links" entry (file+headline org-agenda-file-note "Quick notes")
+         "* TODO [#C] %?\n  %i\n %a \n %U"
+         :empty-lines 1)
+        ("j" "Journal Entry"
+         entry (file+datetree org-agenda-file-journal)
+         "* %?"
+         :empty-lines 1)))
 
 ;;An entry without a cookie is treated just like priority ' B '.
 ;;So when create new task, they are default 重要且紧急
@@ -284,7 +276,6 @@
          ((stuck "") ;; review stuck projects as designated by org-stuck-projects
           (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
           ))))
-
 
 ;; ;; 设置 org-agenda 打开快捷键
 ;; ;; 两个常用 C-c C-s scheduled items
@@ -431,6 +422,7 @@
 ;;下面配置未成功
 (require 'helm-ag)
 (global-set-key (kbd "C-c p s") 'helm-do-ag-project-root)
+(global-set-key (kbd "<f7>") 'helm-do-ag-project-root)
 
 ;;(require 'org-octopress)
 ;;(setq org-octopress-directory-top       "~/Lfy_Study/octopress/source")
@@ -448,15 +440,15 @@
 ;;通过一下配置，可以使用C-w来删除整行（操作时不用事先选中整行）.
 ;;同时不影响原有的剪切功能。M-w也可以在不事先选中整行的情况下复制整行。
 (defadvice kill-ring-save (before slickcopy activate compile)
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2)))))
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+	   (line-beginning-position 2)))))
 (defadvice kill-region (before slickcut activate compile)
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2)))))
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+	   (line-beginning-position 2)))))
 
 ;; 2、优化注释功能
 ;;Emacs的默认设置中，M-;可以起到注释的作用，但是有不方便的地方：必须在选中一部分区域后才能进行注释。
@@ -520,6 +512,7 @@
 
 ;;让 Emacs 可以直接打开和显示图片。
 (setq auto-image-file-mode t)
+
 
 
 (custom-set-variables
